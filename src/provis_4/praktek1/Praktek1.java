3e4bd31ebc3657960fb676b705b52e8b74f26bbf
bintangeditya <bintangfarhandika@gmail.com>
2020-04-15 21:51:22 +0700

    tugas 3 provis praktek 4.1 dan 4.2

---
+++ src/provis_4/praktek1/Praktek1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provis_4.praktek1;
+
+/**
+ *
+ * @author binta
+ */
+public class Praktek1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Praktek1Window mWin = new Praktek1Window();
+         mWin.setSize(500,400);
+         mWin.setVisible(true);    }
+    
+}
---
+++ src/provis_4/praktek2/Praktek2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provis_4.praktek2;
+
+/**
+ *
+ * @author binta
+ */
+public class Praktek2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Praktek2Window mWin = new Praktek2Window();
+         mWin.setSize(500,400);
+         mWin.setVisible(true);  
+    }
+    
+}
