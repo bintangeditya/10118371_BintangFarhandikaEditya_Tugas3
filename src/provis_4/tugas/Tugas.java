2cdf3e7fe07278a6bf1500b9e3e23604d071cc85
bintangeditya <bintangfarhandika@gmail.com>
2020-04-15 23:18:11 +0700

    tugas 3 provis tugas

---
+++ src/provis_4/tugas/Tugas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provis_4.tugas;
+
+/**
+ *
+ * @author binta
+ */
+public class Tugas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         TugasWindow mWin = new TugasWindow();
+         mWin.setSize(300,500);
+         mWin.setVisible(true);  
+    }
+    
+}
